a23a3a08f2b78b5a655786ddea0c95149bc19971
Pavlo Boiko <33000590+PavloYum@users.noreply.github.com>
2024-12-05 22:15:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const box = document.querySelector('.box');
+let x = 0; // Координата left
+let y = 0; // Координата top
+let directionX = 1; // Направление по X (1 — вправо, -1 — влево)
+let directionY = 1; // Направление по Y (1 — вниз, -1 — вверх)
+
+const t = setInterval(move, 20);
+
+function move() {
+  // Изменение координат квадрата
+  x += directionX;
+  y += directionY;
+
+  // Движение вниз и обратно при достижении границ контейнера
+  if (x >= 150 || x <= 0) directionX *= -1; // Меняем направление по X
+  if (y >= 150 || y <= 0) directionY *= -1; // Меняем направление по Y
+
+  // Установка новых координат квадрата
+  box.style.left = `${x}px`;
+  box.style.top = `${y}px`;
+}
 
